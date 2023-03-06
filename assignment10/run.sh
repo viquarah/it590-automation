5dcd0d773695f9c7ae9f0485e42b882d746d85c0
viquarah <98446995+viquarah@users.noreply.github.com>
2023-03-06 06:02:48 +0300

    Update run.sh

--- assignment10/run.sh
+++ assignment10/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo $1 > access
-gcloud compute instances create test-vm --project=adnan-project-374418 --machine-type=e2-medium --network-interface=network=mynew,subnet=mysubnet --zone=us-central1-a --access-token-file=access
+gcloud compute instances create instancexz --project=adnan-project-374418 --machine-type=e2-medium --network-interface=network=mynew,subnet=mysubnet --zone=us-central1-a --access-token-file=access
